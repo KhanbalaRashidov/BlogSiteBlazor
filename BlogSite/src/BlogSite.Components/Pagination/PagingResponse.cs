2641b5157cc7d40038deb43d9a5a1a70bac5879b
Khanbala Rashidov <50279392+KhanbalaRashidov@users.noreply.github.com>
2023-06-28 12:09:37 +0400

    Add Components

---
+++ BlogSite/src/BlogSite.Components/Pagination/PagingResponse.cs
@@ -0,0 +1,10 @@
+﻿using BlogSite.Shared.Features.Pagination;
+
+namespace BlogSite.Components.Pagination
+{
+    public class PagingResponse<T> where T : class
+    {
+        public List<T> Items { get; set; }
+        public Paging Paging { get; set; }
+    }
+}
